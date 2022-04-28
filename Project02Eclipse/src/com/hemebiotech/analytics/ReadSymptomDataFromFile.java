51d5fdc1f42cd62283afc7c9c1578d9ca87b3814
Ter Arb <ty.arbeaumont@gmail.com>
2022-04-28 21:37:50 +0200

    Fixed instances and interfaces

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,3 +1,8 @@
+/**
+*
+*  @author TeArb
+*  @version 1
+*/
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
@@ -6,14 +11,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
+import com.hemebiotech.analytics.interfaces.ISymptomReader;
+
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+	private static ArrayList<String> displaySymptoms = new ArrayList<String>();
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
@@ -21,27 +24,34 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+	/**
+	 * 
+	 * @param display the symptoms
+	 */
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
+	public List<String> getSymptoms() {
+		
+		BufferedReader readSymptomFile;
+		String lineSymptoms;
 		
 		if (filepath != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
+				readSymptomFile = new BufferedReader (new FileReader(filepath));
 				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
+				// Reading all the lines of input text file.
+				while ((lineSymptoms = readSymptomFile.readLine()) != null) {
+					displaySymptoms.add(lineSymptoms);	
+				}	
+								
+				// Release of allocated resources.
+				readSymptomFile.close();
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		return result;
-	}
 
-}
+		return displaySymptoms;
+	} 
+
+}	
